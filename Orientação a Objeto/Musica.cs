4ea2be49c46a835ac558c70c6b08897ef9406aac
kaique correia chagas <80991704+Ckaiique@users.noreply.github.com>
2023-07-16 11:48:21 -0300

    Orientação a Objeto
    Aprendendo como funciona a Orientação a objeto no c#

---
+++ Orientação a Objeto/Musica.cs
@@ -0,0 +1,25 @@
+
+class Musica
+{
+  public string? Nome {get; set;}
+  public string? Artista {get; set;}
+  public int Duracao {get; set;}
+  public bool Disponivel {get; set;}
+  public string NomeCompleto { get; set; }
+
+  public void ExiborFicaTecnica()
+  {
+    Console.WriteLine($"Nome: {Nome}");
+    Console.WriteLine($"Artista: {Artista}");
+    Console.WriteLine($"Duração: {Duracao}");
+    if (Disponivel)
+    {
+      Console.WriteLine($"Disponivel no plano.");
+    }
+    else
+    {
+      Console.WriteLine($"Adquira o plano Plus+");
+    }
+  }
+
+}
---
+++ Orientação a Objeto/Program.cs
@@ -0,0 +1,20 @@
+﻿Musica musica1 = new Musica();
+
+musica1.Nome = "Roxane";
+musica1.Artista = "The Police";
+musica1.Duracao = 273;
+musica1.Disponivel = true;
+Console.WriteLine(musica1.Disponivel);
+
+Musica musica2 = new Musica();
+musica2.Nome = "Vertigo";
+musica2.Artista = "U2";
+musica2.Duracao = 367;
+
+musica2.Disponivel =false;
+Console.WriteLine(musica2.Disponivel);
+
+
+musica1.ExiborFicaTecnica();
+musica2.ExiborFicaTecnica();
+
